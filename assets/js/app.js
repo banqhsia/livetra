60a1120a01b3f56b9dc89a42755f7c55e827c160
Boggy Jang <boggyjan@gmail.com>
2017-06-21 16:21:46 +0800

    加上順行逆行功能

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,21 +27,42 @@ $(function () {
       lines: [],
       stations: [],
       liveBoard: [],
-      timetable: []
+      timetable: [],
+      stationsOfLine: []
     },
     mutations: {
-      increment (state, n=1) {
-        state.count+=n
-      },
       //
-      // 取路線
-      getLines(state) {
-        getData('Line', false, '', json=>state.lines = json);
+      // 取路線後取車站
+      getLinesAndStations(state) {
+        getData('Line', '', false, '', json=>{
+          state.lines = json;
+          store.commit('getStations');
+        });
+      },
+      // 取車站
+      getStations(state) {
+        getData('StationOfLine', '', false, '', json=>{
+          let arr = [];
+
+          json.forEach(item=>{
+            let children = [];
+            item.Stations.forEach(station=>{
+              children.push({text: station.StationName, id: station.StationID})
+            });
+
+            arr.push({
+              text: store.state.lines.filter(i=>i.LineID == item.LineID)[0]['LineNameZh'],
+              children: children
+            });
+          });
+          
+          state.stationsOfLine = arr;
+        }); //取全台車站 (local data)
       },
       //
       // 依照車站取電子看板資料
-      getLiveBoardByStationID(state, id) {
-        getData('LiveBoard', true, "&$filter=StationID eq '" + id + "'", json=>state.liveBoard = json);
+      getLiveBoardByStationID(state, station_id) {
+        getData('LiveBoard', '/' + station_id, true, '', json=>state.liveBoard = json);
         buildLoadingCircle();
       },
       //
@@ -51,7 +72,7 @@ $(function () {
           var d = new Date();
           date = d.getFullYear() + '-' + String(d.getMonth() + 1).padStart(2,'0') + '-' + String(d.getDate()).padStart(2,'0');
         }
-        getData('DailyTimetable/Station/' + station_id + '/' + date, true, '', json=>state.timetable = json);
+        getData('DailyTimetable', '/Station/' + station_id + '/' + date, true, '', json=>state.timetable = json);
       },
       clearTimetable(state) {
         state.timetable = [];
@@ -59,16 +80,45 @@ $(function () {
     }
   });
 
-  function getData(func, origin, params, cb) {
-    // 目前只有Line讓他讀本地資料
+  function getData(func, params = '', origin, filters, cb) {
+    // origin == true 讀ptx pai資料 false 讀本地預先存好的資料（通常就是不太會有變動的部分）
     let path = origin ? 'http://ptx.transportdata.tw/MOTC/v2/Rail/TRA/' : 'data/';
-    fetch(path + func + '?$format=json' + params)
+    path += func + params + '?$format=json' + filters;
+
+    fetch(path)
     .then(data=>data.json())
-    .then(json=>json.filter(item => (item.LineID != 'WL' && item.LineID != 'TL'))) //濾掉西部幹線跟縱貫線（因為這兩條線的LineID無法找到車站
+    .then(json=>{
+      switch (func) {
+        case 'StationOfLine':
+          json = json.filter(item => (item.LineID != 'WL' && item.LineID != 'TL')) //濾掉西部幹線跟縱貫線（因為這兩條線的LineID無法找到車站
+          break;
+        case 'DailyTimetable':
+          var now = new Date().getTime();
+          json = json.filter(item => {
+            let dTime = new Date();
+            dTime.setHours(item.DepartureTime.split(':')[0]);
+            dTime.setMinutes(item.DepartureTime.split(':')[1]);
+            return dTime.getTime() >= now;
+          });
+          break;
+        case 'LiveBoard':
+          var now = new Date().getTime();
+          json = json.filter(item => {
+            let dTime = new Date();
+            dTime.setHours(item.ScheduledDepartureTime.split(':')[0]);
+            dTime.setMinutes(item.ScheduledDepartureTime.split(':')[1]);
+            return dTime.getTime() >= now && item.DelayTime == 0;
+          });
+          break;
+        default:
+          break;
+      }
+      return json;
+    })
     .then(json=>cb(json))
-    .catch(function(data){
+    .catch(data => {
       alert(`資料傳輸錯誤，請重新整理`);
-      //console.log(`${path}${params} 資料傳輸錯誤`);
+      //console.log(func, params, filters, path, origin, '資料傳輸錯誤', data);
     });
   }
 
@@ -90,7 +140,7 @@ $(function () {
     data: {
       selectedStation: localStorage.getItem('selectedStation') || '1008', //預設台北車站
       trainInfo: '',
-      stationsOfLine: []
+      timetableDirection: 0
     },
     watch: {
       selectedStation: function() {
@@ -99,26 +149,8 @@ $(function () {
       }
     },
     created() {
-      store.commit('getLines'); //取線路名稱 (local data)
+      store.commit('getLinesAndStations'); //取線路名稱 (local data)
       store.commit('getLiveBoardByStationID', this.selectedStation);
-      
-      getData('StationOfLine', false, '', json=>{
-        let arr = [];
-
-        json.forEach(item=>{
-          let children = [];
-          item.Stations.forEach(station=>{
-            children.push({text: station.StationName, id: station.StationID})
-          });
-
-          arr.push({
-            text: store.state.lines.filter(i=>i.LineID == item.LineID)[0]['LineNameZh'],
-            children: children
-          });
-        });
-        
-        this.stationsOfLine = arr;
-      }); //取全台車站 (local data)
       
       // 五分鐘自動更新一次
       setInterval(()=>{
@@ -134,7 +166,7 @@ $(function () {
         return trainType[id];
       },
       showTrainInfo: function(trainNo) {
-        getData('GeneralTrainInfo', true, "&$filter=TrainNo eq '" + trainNo + "'", json=>{
+        getData('GeneralTrainInfo', '', true, "&$filter=TrainNo eq '" + trainNo + "'", json=>{
           let data = json[0];
           this.trainInfo = `${data.TrainNo} ${data.TrainTypeName.Zh_tw} ${data.Note.Zh_tw} 由 ${data.StartingStationName.Zh_tw} 開往 ${data.EndingStationName.Zh_tw}`;
           $('#train_info_modal').modal('show');
